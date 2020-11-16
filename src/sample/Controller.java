261a347b42f1615d02b378098546e385840eb12e
Sindhu Adem <sindhuadem@Sindhus-MacBook-Pro.local>
2020-11-15 19:50:42 -0500

    chicken is working, in 10 min everything will work

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.scene.input.MouseEvent;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -33,16 +34,17 @@ public class Controller implements Initializable{
     @FXML
     private TextField price;
 
-    private Chicken chicken_sandwich;
-    private Beef beef_sandwich;
-    private Fish fish_sandwich;
-    ObservableList<String> list = FXCollections.observableArrayList("Beef", "Fish", "Chicken");
-    ObservableList<String> chickenData = FXCollections.observableArrayList("Fried Chicken", "Spicy Sauce", "Pickles");
-    ObservableList<String> beefData = FXCollections.observableArrayList("Roast Beef", "Provolone", "Mustard");
-    ObservableList<String> fishData = FXCollections.observableArrayList("Grilled Snapper", "Cilantro", "Lime");
-    ObservableList<Extra> extraIngredients =
+    private Chicken chicken_sandwich = new Chicken();
+    private Beef beef_sandwich = new Beef();
+    private Fish fish_sandwich = new Fish();
+    private ObservableList<String> list = FXCollections.observableArrayList("Beef", "Fish", "Chicken");
+    private ObservableList<String> chickenData = FXCollections.observableArrayList("Fried Chicken", "Spicy Sauce", "Pickles");
+    private ObservableList<String> beefData = FXCollections.observableArrayList("Roast Beef", "Provolone", "Mustard");
+    private ObservableList<String> fishData = FXCollections.observableArrayList("Grilled Snapper", "Cilantro", "Lime");
+    private ObservableList<Extra> extraIngredients =
             FXCollections.observableArrayList(Extra.LETTUCE, Extra.OLIVES , Extra.CHEDDAR, Extra.TOMATOES,
                     Extra.SWISS, Extra.MUSHROOMS, Extra.JALAPENOS, Extra.MAYO, Extra.SPINACH, Extra.ONIONS);
+
 
 
     @FXML
@@ -59,15 +61,12 @@ public class Controller implements Initializable{
     public void comboChanged(ActionEvent event) {
         if (comboBox.getSelectionModel().getSelectedItem().equals("Chicken")) {
             listView.setItems(chickenData);
-            chicken_sandwich = new Chicken();
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Beef")) {
             listView.setItems(beefData);
-            beef_sandwich = new Beef();
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Fish")) {
             listView.setItems(fishData);
-            fish_sandwich = new Fish();
         }
         else{
             displayArea.appendText("Congrats you broke the combo box!\n");
@@ -78,7 +77,6 @@ public class Controller implements Initializable{
     public void addClicked(MouseEvent event) {
         ObservableList<Extra> extras = lvIngredientSelections.getSelectionModel().getSelectedItems();
         if (comboBox.getSelectionModel().getSelectedItem() == null || comboBox.getSelectionModel().getSelectedItem().equals("Chicken")) {
-            chicken_sandwich = new Chicken();
             for(Extra extra: extras){
                 if(chicken_sandwich.add(extra)) {
                     lvExtraIngredients.getItems().add(extra);
@@ -91,7 +89,6 @@ public class Controller implements Initializable{
             }
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Beef")) {
-            beef_sandwich = new Beef();
             for(Extra extra: extras){
                 if(beef_sandwich.add(extra)) {
                     lvExtraIngredients.getItems().add(extra);
@@ -104,7 +101,6 @@ public class Controller implements Initializable{
             }
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Fish")) {
-            fish_sandwich = new Fish();
             for(Extra extra: extras){
                 if(fish_sandwich.add(extra)) {
                     lvExtraIngredients.getItems().add(extra);
@@ -123,12 +119,13 @@ public class Controller implements Initializable{
 
     @FXML
     public void removeClicked(MouseEvent event) {
-        ObservableList<Extra> extrasRemoved = lvExtraIngredients.getSelectionModel().getSelectedItems();
+        ObservableList<Extra> observalbeextrasRemoved = lvExtraIngredients.getSelectionModel().getSelectedItems();
+        ArrayList<Extra> extrasRemoved = new ArrayList<>(observalbeextrasRemoved);
         if (comboBox.getSelectionModel().getSelectedItem() == null || comboBox.getSelectionModel().getSelectedItem().equals("Chicken")) {
-            chicken_sandwich = new Chicken();
-            for(Extra extraRm: extrasRemoved){
-                if(chicken_sandwich.remove(extraRm)) {
-                    lvExtraIngredients.getSelectionModel().clearSelection();
+            for(int i = 0; i < extrasRemoved.size(); i++){
+                if(chicken_sandwich.remove(extrasRemoved.get(i))) {
+                    lvExtraIngredients.getItems().remove(extrasRemoved.get(i));
+                    //lvExtraIngredients.getSelectionModel().clearSelection();
                     price.setText(String.format("%.2f", chicken_sandwich.price()));
                 }
                 else{
@@ -138,7 +135,6 @@ public class Controller implements Initializable{
             }
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Beef")) {
-            beef_sandwich = new Beef();
             for(Extra extraRm: extrasRemoved){
                 if(beef_sandwich.remove(extraRm)) {
                     lvExtraIngredients.getSelectionModel().clearSelection();
@@ -151,7 +147,6 @@ public class Controller implements Initializable{
             }
         }
         else if (comboBox.getSelectionModel().getSelectedItem().equals("Fish")) {
-            fish_sandwich = new Fish();
             for (Extra extraRm : extrasRemoved) {
                 if (fish_sandwich.remove(extraRm)) {
                     lvExtraIngredients.getSelectionModel().clearSelection();
